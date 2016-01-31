be01b74ef8e7a7083b7f842a6c2a5bd2741325a0
Steve Hatchard <steve@code.je>
2016-01-31 20:06:42 +0000

    Added serialization to Execptions

--- src/app/InvalidEmailAddressException.cs
+++ src/app/InvalidEmailAddressException.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Codentia.Common
 {
     /// <summary>
     /// InvalidEmailAddressException class - exception class for Email Validation
     /// </summary>
+    [Serializable]
     public class InvalidEmailAddressException : ArgumentException
     {
         /// <summary>
@@ -13,6 +15,16 @@ namespace Codentia.Common
         /// <param name="message">Internal message</param>
         public InvalidEmailAddressException(string message)
             : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidEmailAddressException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
+        public InvalidEmailAddressException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
 
--- src/test/ExceptionTest.cs
+++ src/test/ExceptionTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using NUnit.Framework;
 
 namespace Codentia.Common.Types.Test
@@ -7,8 +10,11 @@ namespace Codentia.Common.Types.Test
     /// Unit testing framework for exceptions
     /// </summary>
     [TestFixture]
-    public class ExceptionTest
+    public class ExceptionTest : ISerializable   
     {
+         private SerializationInfo _info;
+         private StreamingContext _context;
+
         /// <summary>
         /// Scenario: Throw exceptions
         /// Expected: Success
@@ -28,6 +34,26 @@ namespace Codentia.Common.Types.Test
                 Assert.That(myex is ArgumentException, Is.True);
             }
 
+            Exception ex = new InvalidEmailAddressException("Message", new Exception("Inner exception."));
+            string exceptionToString = ex.ToString();
+
+            // Round-trip the exception: Serialize and de-serialize with a BinaryFormatter
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // "Save" object state
+                bf.Serialize(ms, ex);
+
+                // Re-use the same stream for de-serialization
+                ms.Seek(0, 0);
+
+                // Replace the original exception with de-serialized one
+                ex = (InvalidEmailAddressException)bf.Deserialize(ms);
+            }
+
+            // Double-check that the exception message and stack trace (owned by the base Exception) are preserved
+            Assert.AreEqual(exceptionToString, ex.ToString(), "ex.ToString()");
+
             Assert.That(delegate { throw new DuplicateEmailAddressException("duplicate email1"); }, Throws.TypeOf<DuplicateEmailAddressException>().With.Message.EqualTo("duplicate email1"));
             Assert.That(delegate { throw new DuplicateEmailAddressException("duplicate email2", new ArgumentException("arg exception")); }, Throws.TypeOf<DuplicateEmailAddressException>().With.Message.EqualTo("duplicate email2"));
 
@@ -40,6 +66,25 @@ namespace Codentia.Common.Types.Test
                 Assert.That(myex is ArgumentException, Is.True);
             }
 
+            bf = new BinaryFormatter();
+            ex = new DuplicateEmailAddressException("Message", new Exception("Inner exception."));
+            exceptionToString = ex.ToString();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // "Save" object state
+                bf.Serialize(ms, ex);
+
+                // Re-use the same stream for de-serialization
+                ms.Seek(0, 0);
+
+                // Replace the original exception with de-serialized one
+                ex = (DuplicateEmailAddressException)bf.Deserialize(ms);
+            }
+
+            // Double-check that the exception message and stack trace (owned by the base Exception) are preserved
+            Assert.AreEqual(exceptionToString, ex.ToString(), "ex.ToString()");
+
             Assert.That(delegate { throw new InvalidPasswordException("invalid password1"); }, Throws.TypeOf<InvalidPasswordException>().With.Message.EqualTo("invalid password1"));
             Assert.That(delegate { throw new InvalidPasswordException("invalid password2", new ArgumentException("arg exception")); }, Throws.TypeOf<InvalidPasswordException>().With.Message.EqualTo("invalid password2"));
 
@@ -51,6 +96,36 @@ namespace Codentia.Common.Types.Test
             {
                 Assert.That(myex is ArgumentException, Is.True);
             }
+
+            bf = new BinaryFormatter();
+            ex = new InvalidPasswordException("Message", new Exception("Inner exception."));
+            exceptionToString = ex.ToString();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // "Save" object state
+                bf.Serialize(ms, ex);
+
+                // Re-use the same stream for de-serialization
+                ms.Seek(0, 0);
+
+                // Replace the original exception with de-serialized one
+                ex = (InvalidPasswordException)bf.Deserialize(ms);
+            }
+
+            // Double-check that the exception message and stack trace (owned by the base Exception) are preserved
+            Assert.AreEqual(exceptionToString, ex.ToString(), "ex.ToString()");
+        }        
+
+        /// <summary>
+        /// Populates a <see cref="T:System.Runtime.Serialization.SerializationInfo" /> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> to populate with data.</param>
+        /// <param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext" />) for this serialization.</param>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            _info = info;
+            _context = context;
         }
     }
 }
