90381f7a355f2e41759e3d94830bf079b6546578
Steve Hatchard <steve@code.je>
2016-11-10 07:40:52 +0000

    Dev version 3.0.0

--- src/app/Properties/AssemblyInfo.cs
+++ src/app/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: InternalsVisibleTo("Codentia.Common.Types.Test")]
